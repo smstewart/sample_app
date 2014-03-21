1f46d0e955e825ed776cce396e7f5941ceca104f
smstewart <smstewart91@gmail.com>
2014-03-20 23:59:02 -0700

    Stable

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,9 @@ let(:base_title) {"Ruby on Rails Tutorial Sample App"}
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("#{base_title}")
     end 
   end
   
